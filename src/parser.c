520d62bf9a96e99d2f62516c7b32911a24fcdaf7
Alexander Mills <alexander.d.mills@gmail.com>
2026-07-24 10:28:26 -0500

    claude autosave 2026-07-24T15:28:26Z

--- src/parser.c
+++ src/parser.c
@@ -5389,7 +5389,7 @@ static void f2e_scan_argv(F2EConfig *config,
 
 /* Dry-run scan that only resolves the command path selected by argv. */
 static void f2e_resolve_command_path(F2EConfig *config, int argc, const char *const argv[], F2ECommandPath *path_out) {
-  f2e_scan_argv(config, NULL, 0, argc, argv, NULL, NULL, NULL, 1, 1, path_out);
+  f2e_scan_argv(config, NULL, 0, argc, argv, NULL, NULL, NULL, 1, 1, 0, path_out);
 }
 
 char *f2e_parse_from_file(const char *config_path, int argc, const char *const argv[]) {
